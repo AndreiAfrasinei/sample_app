95d6414eb536c046e61903ac1a7f3f38f3e81418
AndreiAfrasinei <andrei.afrasinei@assist.ro>
2016-10-12 11:53:04 +0300

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,17 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+              activated: true,
+            activated_at: Time.zone.now.to_datetime)
 
 User.create!(name: "Afrasinei Andrei",
               email: "andrei@ceva.com",
              password: "password",
              password_confirmation: "password",
-             admin:true)
+             admin:true,
+            activated: true,
+            activated_at: Time.zone.now.to_datetime)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -24,5 +28,7 @@ User.create!(name: "Afrasinei Andrei",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+                activated: true,
+                activated_at: Time.zone.now.to_datetime)
 end
